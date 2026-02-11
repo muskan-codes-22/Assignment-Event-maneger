264fef3278e03b1dd01667cf02c9f292255e36bd
muskan-codes-22 <musukumari2211@gmail.com>
2026-02-11 11:43:38 +0530

    Add initial implementation of Event Manager with event creation, display, and deletion features

---
+++ script.js
@@ -0,0 +1,60 @@
+const addBtn = document.getElementById("add-event-btn");
+const eventsList = document.getElementById("events-list");
+const clearBtn = document.getElementById("clear-events");
+const sampleBtn = document.getElementById("sample-events");
+
+// ADD EVENT (To make the add event btn work)
+addBtn.addEventListener("click", () => {
+  const name = document.getElementById("event-name").value;
+  const date = document.getElementById("event-date").value;
+  const desc = document.getElementById("event-description").value;
+
+  if (name === "" || date === "") {
+    alert("Please enter event name and date");
+    return;
+  }
+
+  createEvent(name, date, desc);
+
+  // clear inputs
+  document.getElementById("event-name").value = "";
+  document.getElementById("event-date").value = "";
+  document.getElementById("event-description").value = "";
+});
+
+function createEvent(name, date, desc) {
+  const eventDiv = document.createElement("div");
+  eventDiv.classList.add("event-item");
+
+  eventDiv.innerHTML = `
+    <span class="delete-btn">❌</span>
+    <h4>${name}</h4>
+    <small>${date}</small>
+    <p>${desc}</p>
+  `;
+
+  // delete functionality
+  const deleteBtn = eventDiv.querySelector(".delete-btn");
+  deleteBtn.addEventListener("click", () => {
+    eventDiv.remove();
+  });
+
+  eventsList.appendChild(eventDiv);
+}
+
+// CLEAR ALL EVENTS
+clearBtn.addEventListener("click", () => {
+  eventsList.innerHTML = "";
+});
+// ADD SAMPLE EVENTS
+sampleBtn.addEventListener("click", () => {
+  createEvent("Emifest", "2026-01-14", "Lorem ipsum");
+  createEvent("Project Review", "2026-02-10", "Frontend submission");
+});
+
+// for keys to display
+const keyDisplay = document.getElementById("key-display");
+
+document.addEventListener("keydown", (event) => {
+  keyDisplay.textContent = "You pressed: " + event.key;
+});
